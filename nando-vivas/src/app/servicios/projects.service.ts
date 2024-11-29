e2a6c16de5a2e7538e15fb4568ec13a1c22cf34c
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-11-29 11:42:03 +0100

    Creación de servicios.

---
+++ nando-vivas/src/app/servicios/projects.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Project } from '../interfaces/project';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+  projects$: Observable<Project[]>;
+
+  localProjects: Project[] = [
+    { id: 1, title: 'ARNOLD', client: 'Personal project', category: 'Typography', image: 'assets/works_grid/arnold-personal-work-typegace-serif-art-nouveau-font-nando-vivas-grid-1100x692.png' },
+    { id: 2, title: 'ATTACHED #1 REN HANG', client: 'Personal project', category: 'Editorial', image: 'assets/works_grid/attached-ren-hang-personal-work-editorial-design-nando-vivas-grid-1100x692.png' },
+    { id: 3, title: 'COMUNITZAR LA MÚSICA', client: 'Ajuntament de Barcelona', category: 'Editorial', image: 'assets/works_grid/comunitzar-la-musica-ajuntament-de-barcelona-poster-design-nando-vivas-grid-1100x692.png' },
+    { id: 4, title: 'CREER ES CREAR', client: 'Fundación la Caixa', category: 'Editorial', image: 'assets/works_grid/creer-es-crear-fundacio-la-caixa-illustration-nando-vivas-grid-1100x692.png' },
+    { id: 5, title: 'EDUCAR EN LLAMAS', client: 'Planeta Editorial', category: 'Editorial', image: 'assets/works_grid/educar-en-llamas-planeta-temas-de-hoy-illustration-editorial-design-nando-vivas-grid-1100x692.png' },
+    { id: 6, title: 'FOOT TWIN', client: 'Markus Nebel', category: 'Illustration', image: 'assets/works_grid/foottwin-markus-nebel-mascot-illustration-nando-vivas-grid-1100x692.png' },
+    { id: 7, title: 'FUSION', client: 'Fervor Films', category: 'Branding', image: 'assets/works_grid/fusion-fervor-films-lettering-poster-credits-design-nando-vivas-grid-1100x692.png' },
+    { id: 8, title: 'KIMLY PARK INSTANT COFFEE', client: 'Kimly Parc, Seoul', category: 'Packaging', image: 'assets/works_grid/instant-coffe-kimly-parc-packaging-illustration-nando-vivas-grid-1100x692.png' },
+    { id: 9, title: 'MEMORIA VIVA', client: 'Ajuntament de Barcelona', category: 'Illustration', image: 'assets/works_grid/memoria-viva-ajuntament-de-barcelona-illustration-poster-infographics-nando-vivas-grid-1100x692.png' },
+    { id: 10, title: 'MESTER', client: 'University of California Los Angeles UCLA', category: 'Editorial', image: 'assets/works_grid/mester-UCLA-editorial-design-cover-nando-vivas-grid-1100x692.png' },
+    { id: 11, title: 'PLOU', client: 'Plou Estudi', category: 'Branding', image: 'assets/works_grid/plou-estudi-ceramic-studio-valencia-branding-nando-vivas-grid-1100x692.png' },
+    { id: 12, title: 'RADIO CULTURA VIVA', client: 'Ajuntament de Barcelona', category: 'Web & SM', image: 'assets/works_grid/radio-cultura-viva-ajuntament-de-barcelona-branding-social-media-nando-vivas-grid-1100x692.png' },
+    { id: 13, title: 'RECERQUES CULTURA VIVA', client: 'Ajuntament de Barcelona', category: 'Editorial', image: 'assets/works_grid/recerques-cultura-viva-ajuntament-de-barcelona-editorial-design-nando-vivas-grid-1100x692.png' },
+    { id: 14, title: 'VAPOR XX', client: 'Personal work', category: 'Typography', image: 'assets/works_grid/vapor-x-personal-work-illustration-toys-sculpture-nando-vivas-grid-1100x692.png' },
+    { id: 15, title: 'WELLCOME BACK COVID KIT', client: 'Le Meridien Hotel', category: 'Packaging', image: 'assets/works_grid/welcome-back-covid-kit-educo-hotel-le-meridien-packaging-illustration-nando-vivas-grid-1100x692.png' }
+  ];
+ 
+
+  constructor(private firestore: Firestore) {
+    const projectsCollection = collection(this.firestore, 'projects');
+    this.projects$ = collectionData(projectsCollection, { idField: 'id' }).pipe(
+      catchError(error => {
+        console.error("Error al conectar con Firebase, usando datos locales", error);
+        // Retorna los datos locales en caso de error
+        return of(this.localProjects);
+      })
+    );
+  }
+
+  getProjects(): Observable<Project[]> {
+    return this.projects$;
+  }
+}
