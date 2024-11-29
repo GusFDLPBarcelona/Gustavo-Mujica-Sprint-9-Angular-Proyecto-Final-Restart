934a88905099f32c0361dbdc2d5e8e9bfdf71e90
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-11-29 11:54:00 +0100

    Creación y ajustes de rutas en app.routes.ts

--- nando-vivas/src/app/app.routes.ts
+++ nando-vivas/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ShopComponent } from './componentes/shop/shop.component';
+import { DetailComponent } from './componentes/detail/detail.component';
+import { WellcomeComponent } from './componentes/wellcome/wellcome.component'; // Importa WellcomeComponent
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WellcomeComponent },  // Ruta raíz para mostrar "wellcome" como pantalla de inicio
+    { path: 'work', component: HomeComponent },  // Ruta para "work"
+    { path: 'contact', component: FooterComponent },  // Ruta para "contact" en footer
+    { path: 'shop', component: ShopComponent },  // Ruta para "shop"
+    { path: 'detail/:id', component: DetailComponent }, // Ruta para el detalle de un proyecto.
+    { path: '**', redirectTo: ''}, // Ruta por defecto
+];
