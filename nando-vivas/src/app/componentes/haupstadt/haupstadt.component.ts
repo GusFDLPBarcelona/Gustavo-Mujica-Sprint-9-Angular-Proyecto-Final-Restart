e97c3f0a2177cf1ebd5ee32f3c0c83351689f972
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-12-02 17:15:40 +0100

    correccines standalone: true en componentes.

--- nando-vivas/src/app/componentes/haupstadt/haupstadt.component.ts
+++ nando-vivas/src/app/componentes/haupstadt/haupstadt.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-haupstadt',
+    standalone: true,
     imports: [],
     templateUrl: './haupstadt.component.html',
     styleUrl: './haupstadt.component.css'
